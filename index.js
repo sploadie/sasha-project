85a43860ed53f48c77b549192ee5b4e8f97c278b
Tanguy Gauvrit <tanguy@pandascore.co>
2019-04-15 11:03:47 +0200

    Added Sasha's work

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .post("/save_canvas", (req, res) => {
+    console.log(req.body);
+    res.send("POST request to save_canvas");
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
